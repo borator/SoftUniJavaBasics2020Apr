a7d4bb0d3f90a2ed082f92f4d1d1dab2dc607837
borator <borator@abv.bg>
2020-05-18 23:49:28 +0300

    coin join end

--- Exercise6_20200517/src/com/rado/cake.java
+++ Exercise6_20200517/src/com/rado/cake.java
@@ -1,7 +1,27 @@
 package com.rado;
 
+import java.util.Scanner;
+
 public class cake {
     public static void main(String[] args) {
-        
+        Scanner sca = new Scanner(System.in);
+        int a = Integer.parseInt(sca.nextLine());
+        int b = Integer.parseInt(sca.nextLine());
+        int tmpPeaces = 0;
+        int peacesCount = a*b;
+        String tmp;
+        while(peacesCount>=0){
+            tmp = sca.nextLine();
+            if("STOP".equals(tmp)){
+                System.out.printf("%d pieces are left.",peacesCount);
+                break;
+            }
+            tmpPeaces = Integer.parseInt(tmp);
+            if(peacesCount-tmpPeaces<0){
+                System.out.printf("No more cake left! You need %d pieces more.",Math.abs(peacesCount-tmpPeaces));
+                break;
+            }
+            peacesCount -= tmpPeaces;
+        }
     }
 }
